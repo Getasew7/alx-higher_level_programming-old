ca39197fbe5eb7b358f274e999e3a2ad05a394a5
Getasew Endaylalu <117802485+Getasew7@users.noreply.github.com>
2023-03-14 22:47:09 +0300

    task 0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -3,7 +3,6 @@
 if __name__ == "__main__":
     '''print the sum of 1 and 2'''
     from add_0 import add
-    
     a = 1
     b = 2
-    print(f"{a} + {b} = {a, b}".format(a, b, add(a, b)))
+    print(f"{} + {} = {}".format(a, b, add(a, b)))
